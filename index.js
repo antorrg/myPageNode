cf2f534aeb4f1887a3dd0ed388d9e77d434c5d6a
antorrg <antoniorodriguezgramajo@gmail.com>
2024-08-09 01:48:14 -0300

    commit update

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
+//todo o    o        8 8           .oPYo.               
+//todo 8    8        8 8           8    8              
+//todo o8oooo8 .oPYo 8 8 .oPYo    o8YooP' o    o  .oPYo. 
+//todo 8    8 8oooo8 8 8 8    8    8      8    8 8      8
+//todo 8    8 8.     8 8 8    8    8      8    8 8      `
+//todo 8    8 `Yooo' 8 8 `YooP'    8      `YooP' `YooP8o8
+//todo                                                  8
+//todo                                             ooooP'
+//*:::::: Api MVC creada con express.js sqlite y motor de plantillas PUG ::
+//*:::::: Actualizada el 09/08/2024 :::::::::::::::::::::::::::::::
 import app from './src/app.js'
 import * as sqlite from './src/db.js'
 import s from './src/envConfig.js'
